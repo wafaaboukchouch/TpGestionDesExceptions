0e0d2101768d3327a58db40d07ef7ed5072b10a4
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-18 19:13:43 +0200

    Update NombreNegatifException.java

--- src/main/java/NombreNegatifException.java
+++ src/main/java/NombreNegatifException.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
-public class NombreNegatifException {
+public class NombreNegatifException extends Exception {
+    private int valeurErronee;
+    
+    // Constructeur avec message et valeur erronée
+    public NombreNegatifException(String message, int valeur) {
+        super(message);
+        this.valeurErronee = valeur;
+    }
+    
+    // Constructeur simple
+    public NombreNegatifException(int valeur) {
+        super("Erreur : nombre négatif interdit ! Valeur : " + valeur);
+        this.valeurErronee = valeur;
+    }
+    
+    // Méthode pour consulter la valeur erronée
+    public int getValeurErronee() {
+        return valeurErronee;
+    }
 }
