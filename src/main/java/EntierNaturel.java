d12e868f1fd7c007d354e268e78d904d8a1c2d03
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-18 19:13:03 +0200

    Update EntierNaturel.java

--- src/main/java/EntierNaturel.java
+++ src/main/java/EntierNaturel.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class EntierNaturel {
+    private int val;
+    
+    // Constructeur
+    public EntierNaturel(int val) throws NombreNegatifException {
+        if (val < 0) {
+            throw new NombreNegatifException(val);
+        }
+        this.val = val;
+    }
+    
+    // Accesseur en lecture
+    public int getVal() {
+        return val;
+    }
+    
+    // Accesseur en écriture
+    public void setVal(int val) throws NombreNegatifException {
+        if (val < 0) {
+            throw new NombreNegatifException(val);
+        }
+        this.val = val;
+    }
+    
+    // Méthode de décrémentation
+    public void decrementer() throws NombreNegatifException {
+        if (val - 1 < 0) {
+            throw new NombreNegatifException(val - 1);
+        }
+        val--;
+    }
+    
+    @Override
+    public String toString() {
+        return "EntierNaturel{" + "val=" + val + '}';
+    }
 }
